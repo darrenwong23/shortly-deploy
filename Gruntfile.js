26f99b4964661b89ca727cd764fbc18334eddbe4
Hack Reactor Students <accounts@hackreactor.com>
2015-04-24 16:29:14 -0700

    finished configfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,29 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';',
+        ignores: [
+          'public/dist/**/*.js'
+        ]
+      },
+      dist: {
+        // the files to concatenate
+        src: ['public/**/*.js', 'test/**/*.js', '/app/**/*.js'],
+        // the location of the resulting JS file
+        dest: 'public/dist/<%= pkg.name %>.js'
+      }
     },
 
     mochaTest: {
       test: {
+
         options: {
-          reporter: 'spec'
+          reporter: 'spec',
+          force: 'true'
         },
         src: ['test/**/*.js']
       }
@@ -21,12 +37,22 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+        ignores: [
+          'public/lib/**/*.js',
+          'public/dist/**/*.js'
+        ]
+      },
+      dist: {
+        files: {
+          'public/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
     },
 
     jshint: {
-      files: [
-        // Add filespec list here
-      ],
+      files: ['Gruntfile.js', 'public/**/*.js', 'test/**/*.js', 'app/**/*.js'],
       options: {
         force: 'true',
         jshintrc: '.jshintrc',
@@ -38,6 +64,15 @@ module.exports = function(grunt) {
     },
 
     cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'public/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/dist',
+          ext: '.min.css'
+        }]
+      }
     },
 
     watch: {
@@ -58,8 +93,12 @@ module.exports = function(grunt) {
     },
 
     shell: {
-      prodServer: {
-      }
+
+        target: {
+            command: 'git push azure master'
+        }
+        //probably something goes in here
+
     },
   });
 
@@ -94,19 +133,35 @@ module.exports = function(grunt) {
   ]);
 
   grunt.registerTask('build', [
+    //something goes in here
+    'concat',
+    'uglify',
+    'cssmin',
+    'nodemon'
+
   ]);
 
-  grunt.registerTask('upload', function(n) {
+  // grunt.registerTask('upload', function(n) {
+  //   if(grunt.option('prod')) {
+  //     // add your production server task here
+  //   } else {
+  //     grunt.task.run([ 'server-dev' ]);
+  //   }
+  // });
+
+  grunt.registerTask('deploy', function(n) {
+    // add your deploy tasks here
     if(grunt.option('prod')) {
-      // add your production server task here
+      'concat',
+      'uglify',
+      'cssmin',
+      'mochaTest'
+      'shell'
     } else {
       grunt.task.run([ 'server-dev' ]);
     }
-  });
+  } );
 
-  grunt.registerTask('deploy', [
-    // add your deploy tasks here
-  ]);
 
 
 };
